8ef6d5b1dcb94a17ff3d753d592454ae33fa9b6f
Andres <roger@roger.dev>
2026-02-09 14:52:01 -0500

    Task ended

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+# Lab TIFC1-PF-3-3---Lab-Build-a-Better-Calculator
+
+def addmultiplenumbers(nums):
+    total = 0
+    for num in nums:
+      total += num
+    return total
+
+def multiplymultiplenumbers(nums):
+    total = 1
+    for num in nums:
+      total *= num
+    return total
+
+def isiteven(num):
+   return num % 2 == 0
+
+def isitaninteger(num):
+   if type(num) == int:
+      return True
+   else:
+      return False
+
 def main():
-  print("Hello learners!")
+   print(f"El resultado de la suma es: {addmultiplenumbers([1, 2, 3])}")
+   print(f"El resultado de la multiplicacion es: {multiplymultiplenumbers([1, 2, 3])}")
+   print(isiteven(2))
+   print(isitaninteger(2.5))
+
 
 if __name__=="__main__":
   main()
